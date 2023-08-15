d992a3f9dd26fedb8daed053d971530b1d24efd3
emiliano soba <emisoba97@gmail.com>
2023-08-15 18:36:54 -0300

    Solucion-click

--- js/eventos.js
+++ js/eventos.js
@@ -2,7 +2,18 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let boton = document.getElementById("boton");
 
-boton.addEventListener("click", function() {
+
+boton.addEventListener('click', function(event) {
+    event.stopPropagation();
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+let div = document.getElementById("div");
+
+div.addEventListener("click", function() {
     alert("Hola! Soy el div");
+
 });
 });
